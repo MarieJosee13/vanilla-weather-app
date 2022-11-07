bb6fd51aa4c29f9d59c79eec91c628078b0db840
MarieJosee13 <mariejosee1313@gmail.com>
2022-11-07 09:36:35 -0800

    added the search engine with JS

--- src/app.js
+++ src/app.js
@@ -41,9 +41,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+function search(city) {
+  let apiKey = "5e910c08188e49716f20c4b9bf7bd81f";
 
-let apiKey = "5e910c08188e49716f20c4b9bf7bd81f";
-let city = "Vancouver";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("Vancouver");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
